643b3c0a775b76e417165e5ee0dc9fd4e108c388
Haner Camilo Perea Yara <hanercamilo@hotmail.com>
2025-09-16 17:48:21 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       method: req.method,
       headers: {
         ...req.headers, // 🔥 reenvía los headers que mandó tu cliente
-        host: undefined // evita problemas con el header `host`
+        host: undefined, // evita problemas con el header `host`
       },
       body:
         req.method !== "GET" && req.method !== "HEAD"
@@ -18,12 +18,12 @@ export default async function handler(req, res) {
     });
 
     const contentType = response.headers.get("content-type");
+    const contentEncoding = response.headers.get("content-encoding");
     const buffer = await response.arrayBuffer(); // soporta JSON, binarios, etc.
 
     res.setHeader("Access-Control-Allow-Origin", "*");
-    if (contentType) {
-      res.setHeader("Content-Type", contentType);
-    }
+    if (contentType) res.setHeader("Content-Type", contentType);
+if (contentEncoding) res.setHeader("Content-Encoding", contentEncoding);
 
     res.status(response.status).send(Buffer.from(buffer));
   } catch (error) {
